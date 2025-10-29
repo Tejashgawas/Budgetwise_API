a29cda1c231316cb24bf4ed61ebd5895e6a7bd9e
Tejashgawas <gawastejash@gmail.com>
2025-10-29 16:29:16 +0530

    Added Auth Module and Other fixes

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,3 +1,38 @@
+from pydantic import BaseModel, EmailStr,Field
+from typing import Optional
+
+class RegisterSchema(BaseModel):
+    username: str = Field(..., min_length=3, max_length=80)
+    email: EmailStr
+    password: str = Field(..., min_length=6)
 
 
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str
 
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class RegisterResponse(BaseModel):
+    message: str
+    user: UserResponse
+
+
+class LoginResponse(BaseModel):
+    message: str
+    token: str
+    user: UserResponse
+
+class UserDetailResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: str
+
+
+class LogoutResponse(BaseModel):
+    message: str
