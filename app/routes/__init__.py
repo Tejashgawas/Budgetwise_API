765ce35ebe75f67f06cc3cbc7aac9b191f5a5b66
YadneshGawas <yadnesh.gawas@gmail.com>
2025-10-31 12:32:03 +0530

    Import CSV added

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from app.routes.auth_routes import auth_bp
 from app.routes.transaction_routes import transaction_bp
 from app.routes.category_routes import category_bp
 from app.routes.summary_routes import summary_bp
+from app.routes.csv_routes import csv_bp
 
 # Master blueprint (optional grouping)
 api_bp = Blueprint("api", __name__)
@@ -17,3 +18,4 @@ api_bp.register_blueprint(auth_bp, url_prefix="/auth")
 api_bp.register_blueprint(transaction_bp, url_prefix="/transactions")
 api_bp.register_blueprint(category_bp, url_prefix="/categories")
 api_bp.register_blueprint(summary_bp, url_prefix="/summary")
+api_bp.register_blueprint(csv_bp, url_prefix="/csv")
